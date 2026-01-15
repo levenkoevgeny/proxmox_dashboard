4d8a3fb645cf68b18770c6640bdbb248d29db0f1
evgeny <levenko.evgeny@yandex.by>
2026-01-15 14:28:17 +0300

    fix

--- proxmoxDashboard/settings.py
+++ proxmoxDashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ioa8^xti^7jo(%vr*3%qi(&-@cu35vjjjs24dtywm9r_31t))3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "pms.amvd.by"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
